8672c5067643bc5370845672bcfe4f7af19b5bc2
Om Waghmare <omwaghmare101@gmail.com>
2026-06-30 23:46:57 +0530

    question15_done

---
+++ If_Else.py
@@ -0,0 +1,51 @@
+# '''
+# 11. Wap to check whether the data is mutable or not. 
+# '''
+# a=eval(input("enter data"))
+# if type(a) in[list,set,dict]:
+#     print("data is mutable")
+# else:
+#     print("data is immutable")
+
+
+# '''
+# 12. Wap to check whether the given character is digit or not. 
+# '''
+# a=eval(input("enter data"))
+# if type(a)==int:
+#     print("character is digit")
+# else:
+#     print("not digit")
+
+
+# ''''
+# 13. Wap to check whether the given character is special or not. 
+# '''
+# a=input("enter data")
+# if a in "!@#$%^&*()":
+#     print("Spacial")
+# else:
+#     print("Not Special")
+
+
+# '''
+# 4. Wap to check whether a list consists of middle value or not. 
+# '''
+# a=eval(input("ENTER DATA "))
+# if len(a)%2==1:
+#     print("Have middle values")
+# else:
+#     print("dont have middle values")
+
+
+# '''
+# 5. Wap to check whether the number is even or odd. 
+# '''
+# a= int(input("ENTER A NUMBER "))
+# if a%2==0:
+#     print("EVEN")
+# else:
+#     print("ODD")
+
+
+
---
+++ SimpleIf.py
@@ -0,0 +1,93 @@
+# '''
+# Q1)Wap to print the square of a number only if it is even.
+# '''
+# a = int(input("Enter a number"))
+# if a%2==10:
+#     print(a**2 )
+
+
+
+# '''
+# Q2)Wap to check whether the character is vowel or not. 
+# '''
+# a = str(input("Enter a Alphabet"))
+# if a in "aeiouAEIOU":
+#     print(a , "is an Vowle")
+
+
+
+# '''
+# Q3)Wap to print Ascii value of a character only if it is upper case. 
+# '''
+# a = str(input("Enter a Alphabet"))
+# if 65<=ord(a)<=90:
+#     print(ord(a))
+
+# a = str(input("Enter a Alphabet"))
+# if 'A'<=a<='Z':
+#     print(ord(a))
+
+
+
+# '''
+# Q4)Wap to print the cube of a number only if it is divisible by 9 or 6. 
+# '''
+# a = int(input("Enter a number"))
+# if a%3==0 or a%9==0:
+#     print(a**3)
+
+
+# '''
+# Q5)Wap to check whether the given integer is 3 Digit number.
+# ''' 
+# a = str(input("Enter THREE DIGIT number: "))
+# if len(a)==3:
+#     print("its an three digit number")
+
+
+
+# '''
+# Q6)Wap to check whether the last digit of a given number is 5.
+# '''
+# a = int(input("Enter a number"))
+# b=a%10
+# if b==5 :
+#     print("digit is 5")
+
+
+
+# '''
+# Q7)Wap to check whether the given data is float.
+# '''
+# a =float(input("Enter Data"))
+# if type(a)== float:
+#     print("data is float")
+
+
+
+# '''
+# Q8)Wap to check whether the data is single value data.
+# '''
+# a =input("Enter Data")
+# if len(a)==1:
+#     print("single digit")
+
+
+
+# '''
+# Q9)Wap to check whether the given character is digit or not. 
+# '''
+# a =int(input("Enter Data"))
+# if 0<=a<=9:
+#     print("its an digit")
+
+
+
+# '''
+# Q 10)Wap to check whether the given integer is multiple of 3.
+# '''
+# a =int(input("Enter Data"))
+# if a %3==0:
+#     print("Divisible by 3")
+
+
