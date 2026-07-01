d6d5e755752e8c34cbaf0caf69cd6ad9168aa06f
OM WAGHMARE <omwaghmare101@gmail.com>
2026-07-01 21:37:52 +0530

    questions20_solved

--- If_Else.py
+++ If_Else.py
@@ -48,4 +48,53 @@
 #     print("ODD")
 
 
+# '''
+# 16. Wap to check whether the given data is mutable or immutable. 
+# '''
+# a = eval(input("Enter Data: "))
+# if type(a) in [list,set,dict]:
+#     print("Data is Mutable")
+# else:
+#     print("Data is Unmutable")
 
+
+
+# '''
+# 17. Wap to check whether 2 values are pointing to the same memory or not. 
+# '''
+# a = eval(input("Enter first Data: "))
+# b = eval(input("Enter second Data: "))
+# if id(a) == id(b):
+#     print("Same Memory Location")
+# else:
+#     print("Different Memory Location")
+
+
+# '''
+# 18. Consider a tuple of length 2 and check whether the tuple is homogenous or not.
+# '''
+# a = eval(input("Enter Tuple Data: "))
+# if type(a[0]) == type(a[1]):
+#     print("tuple is homogenous")
+# else:
+#     print("tuple is heterogenous")
+
+
+# '''
+# 19. Wap to check whether the string is palindrome or not. 
+# '''
+# a = eval(input("Enter String Data: "))
+# b = a[::-1]
+# if a==b:
+#     print("data is palindrome")
+# else:
+#     print("data is not palindrome")
+
+# '''
+# 20. Wap to check whether the number is positive or negative.
+# '''
+# a = eval(input("Enter Number : "))
+# if a<0:
+#     print("Number is negative")
+# else:
+#     print("number is positive")
