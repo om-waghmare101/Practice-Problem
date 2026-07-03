64873379927971a1a7ba3e101b972ce882185321
OM WAGHMARE <omwaghmare101@gmail.com>
2026-07-03 20:58:42 +0530

    nestedif2_solved

---
+++ Nested_If.py
@@ -0,0 +1,74 @@
+# # 29.Wap to login into the Instagram with valid username and password.(enter       
+# #     password only if the user name is valid) 
+
+# user = "user@123"
+# pas = "pas@12"
+# entry = input("Enter User ID :")
+# if entry == user :
+#     pasentry = input("Enter Password : ")
+#     if pasentry == pas :
+#         print("Login Sucessfull!!!")
+#     else:
+#         print("Incorrect Password")
+# else:
+#     print("Incorrect User ID")
+
+
+
+# 30. Wap to print the middle value of a list only if it is string. 
+a =eval (input("Enter a list : "))
+if len(a) %2==1:
+    print("Have middle value")
+    b = len(a)//2 
+    if type(a[b])== str:
+        print("its an string and value is ",b)
+    else:
+        print("not a string")
+else:
+    print("Dont Have middle value")
+
+
+
+
+# 31.Wap to check whether the character is vowel or consonant. 
+
+
+
+
+
+# 32.Wap to find the greatest of 4 numbers. 
+
+
+
+
+# 33. Wap to print the value as it is only if the length of the value is even. 
+
+
+
+
+# 34.Wap to print the last value of a list only if it is palindrome string starting with   
+#     vowel. 
+
+
+
+
+# 35.Wap to print the reversed string only if it is starting with vowel ,ending with     
+#     consonant and having a middle value. 
+
+
+
+
+
+# 36.Wap to find the second greatest of 4 values. 
+
+
+
+
+# 37.Wap to find the smallest of 4 numbers. 
+
+
+
+
+
+# 38. Write a program to print middle Character of the given string only if it is upper                
+#     Case Character.
