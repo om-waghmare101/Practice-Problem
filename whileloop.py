65a656c0d661ccaa710f6dfb476231ad08c794f1
OM WAGHMARE <omwaghmare101@gmail.com>
2026-07-06 22:48:31 +0530

    whileStart

---
+++ whileloop.py
@@ -0,0 +1,38 @@
+# #  39.Wap to print python for 5 times. 
+# n = 1
+# while (n<=5):
+#     print("PYTHON")
+#     n+=1
+
+#  40.Wap to print n natural numbers. 
+#  41.Wap to print multiplication table for n. 
+#  42.Wap to find the sum of n natural numbers. 
+#  43. Wap to find the product of n natural numbers or factorial of a number. 
+#  44.Wap to print all the characters of a string. 
+#  45.Wap to print all the characters present at even index of a string. 
+#  46.Wap to extract all the lowercase characters present in a string. 
+#  47.Wap to extract all the vowels present in a string. 
+#  48.Wap to print factors of a integer number. 
+#  49.Wap to toggle a string. 
+#  50.Wap to reverse the given number. 
+#  51.Wap to find the sum of individual digits of a number. 
+#  52. Wap to check whether the number is perfect or not. 
+#  53.Wap to login to phonepe by entering correct otp. 
+#  54.Wap to run infinite loop until user enters the correct password. 
+#  55.Wap to extaract all the even integers present in a tuple at odd index. 
+#  56.Wap to remove duplicates from a list without converting into set. 
+#  57.Wap to find the sum of all the odd numbers between the given range. 
+#  58.Wap to find the greatest number in a given list of integers. 
+#  59.Wap to find the sum of cube of a number in a string. 
+#  60.Wap to check whether the number is Armstrong or not. 
+#  61.Wap to get the following output. 
+#     A=’10011100’   B=’00110101’    out=4(count of positions having same values) 
+#  62.Wap to check the given number is prime or not. 
+#  63.Wap  to check whether the number is palindrome or not. 
+#  64.Wap to find the HCF of two numbers. 
+#  65.Wap to convert binary to decinaml. 
+#  66. Wap to convert decimal to binary. 
+#  67.Wap to count the number of words in a string. 
+#  68.Wap to guess the number. 
+#  69.Wap to find the common elements in two sets 
+#  70.Wap to find the product of all the digits present in a number. 
