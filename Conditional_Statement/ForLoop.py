69063936ca95ebc88e7ecefc71ea19cf2a052515
OM WAGHMARE <omwaghmare101@gmail.com>
2026-07-13 01:33:23 +0530

    MadeCS_Folder

---
+++ Conditional_Statement/ForLoop.py
@@ -0,0 +1,87 @@
+#  71.Wap to print all the integers present in a list. 
+
+
+
+
+
+#  72.Wap to find the length of homogenous tuple without len(). 
+
+
+
+
+#  73.Wap to extract all the even numbers present in a list. 
+
+
+
+
+#  74.Wap to remove duplicates from list  
+
+
+
+
+
+#  75.Wap to reverse a string without using slicing. 
+
+
+
+
+
+
+#  76.wap to extract all the lowercase characters in a string only if the ascii value is  
+#     even.  
+
+
+
+
+#  77.Wap to check whether the last digit of an integer is even or not. 
+
+
+
+#  78.Wap to extract all the key value pairs from the dictionary only if the keys are of  
+#    string datatype and values are integers. 
+
+
+
+
+#  79.Wap to extract key value pairs from the dictionary only if both keys and values 
+#     are exactly same. 
+
+
+
+
+#  80.Wap to get the following output using len function. 
+#      S=’power star’ 
+#      Out={‘power’:5,’star’:4} 
+
+
+
+
+#  81.Wap to get the following output. 
+#      S=’power star’ 
+#      Out={‘power’:’rewop’,’star’:’rats’} 
+
+
+
+
+#  82. wap to extract all the non default  values from a list. 
+
+
+
+
+#  83.Wap to check whether the list is homogenous or not. 
+
+
+
+#  84.Wap to replace the space by * present in a string 
+
+
+
+#  85.Wap to count the number of occurrence of a specified character. 
+
+
+
+
+
+#        86. Wap to get the following output. 
+#                     S=’always keep smiling’ 
+#                     Out-‘syawla peek gnilims’ 
---
+++ Conditional_Statement/Nested_If.py
@@ -0,0 +1,226 @@
+# # 29.Wap to login into the Instagram with valid username and password.(enter       
+# #     password only if the user name is valid) 
+
+# user = "user@123"
+# pas = "pas@12"
+# entry = input("Enter User ID :")
+# if entry == user :
+#     pasentry = input("Enter Password : ")
+#     if pasentry == pas :
+#         print("Login Sucessfull!!!")
+#     else:
+#         print("Incorrect Password")
+# else:
+#     print("Incorrect User ID")
+
+
+
+# # 30. Wap to print the middle value of a list only if it is string. 
+# a =eval (input("Enter a list : "))
+# if len(a) %2==1:
+#     print("Have middle value")
+#     b = len(a)//2 
+#     if type(a[b])== str:
+#         print("its an string and value is ",b)
+#     else:
+#         print("not a string")
+# else:
+#     print("Dont Have middle value")
+
+
+
+
+# # 31.Wap to check whether the character is vowel or consonant. 
+# a = input("Enter character : ")
+# if "a"<=a<="z" or "A" <= a <="Z":
+#     if a in "aeiou":
+#         print("Its an Vowel")
+#     else:
+#         print("Its an consonant")
+# else:
+#     print("Incorrect character")
+
+
+
+
+
+# # 32.Wap to find the greatest of 4 numbers. 
+# a = int(input("Enter Number 1: "))
+# b = int(input("Enter Number 2: "))
+# c = int(input("Enter Number 3: "))
+# d = int(input("Enter Number 4: "))
+
+# if a >= b:
+#     if a >= c:
+#         if a >= d:
+#             print("First number is greatest")
+#         else:
+#             print("Fourth number is greatest")
+#     else:
+#         if c >= d:
+#             print("Third number is greatest")
+#         else:
+#             print("Fourth number is greatest")
+# else:
+#     if b >= c:
+#         if b >= d:
+#             print("Second number is greatest")
+#         else:
+#             print("Fourth number is greatest")
+#     else:
+#         if c >= d:
+#             print("Third number is greatest")
+#         else:
+#             print("Fourth number is greatest")
+
+
+# # 33. Wap to print the value as it is only if the length of the value is even. 
+# a = eval(input("Enter value: "))
+# if type(a) in [str, list, tuple, set, dict]:
+#     if len(a) % 2 == 0:
+#         print(a)
+#     else:
+#         print("Length is odd")
+# else:
+#     print("This value has no length")
+
+
+
+
+
+# # 34.Wap to print the last value of a list only if it is palindrome string starting with   
+# #     vowel. 
+# a = eval(input("ENTER PROPER DATA: "))
+# b = a[-1][::-1]
+
+# if a[-1]==b:
+#         if a[-1][0] in"AEIOUaeiou":
+#             print(a[-1])
+#         else :
+#               print("Not a vowel")
+# else:
+#       print("Not palindrome")
+
+
+
+
+# # 35.Wap to print the reversed string only if it is starting with vowel ,ending with     
+# #     consonant and having a middle value. 
+# a = input("Enter String : ")
+# if a[0].lower() in "aeiou":
+#     if a[-1].isalpha() and a[-1].lower() not in "aeiou":
+#         if len(a)%2==1:
+#             print(a[::-1])
+#         else :
+#             print("Dont have middle value")
+#     else:
+#        print("last value is not an consonant  ") 
+# else:
+#     print("first value is not a vowel")
+
+
+
+
+
+# # 36.Wap to find the second greatest of 4 values. 
+# a = int(input("Enter Number 1: "))
+# b = int(input("Enter Number 2: "))
+# c = int(input("Enter Number 3: "))
+# d = int(input("Enter Number 4: "))
+
+# if a >= b and a >= c and a >= d:
+#     if b >= c:
+#         if b >= d:
+#             print("Second greatest is:", b)
+#         else:
+#             print("Second greatest is:", d)
+#     else:
+#         if c >= d:
+#             print("Second greatest is:", c)
+#         else:
+#             print("Second greatest is:", d)
+
+# elif b >= a and b >= c and b >= d:
+#     if a >= c:
+#         if a >= d:
+#             print("Second greatest is:", a)
+#         else:
+#             print("Second greatest is:", d)
+#     else:
+#         if c >= d:
+#             print("Second greatest is:", c)
+#         else:
+#             print("Second greatest is:", d)
+
+# elif c >= a and c >= b and c >= d:
+#     if a >= b:
+#         if a >= d:
+#             print("Second greatest is:", a)
+#         else:
+#             print("Second greatest is:", d)
+#     else:
+#         if b >= d:
+#             print("Second greatest is:", b)
+#         else:
+#             print("Second greatest is:", d)
+
+# else:
+#     if a >= b:
+#         if a >= c:
+#             print("Second greatest is:", a)
+#         else:
+#             print("Second greatest is:", c)
+#     else:
+#         if b >= c:
+#             print("Second greatest is:", b)
+#         else:
+#             print("Second greatest is:", c)
+
+
+
+
+
+# # 37.Wap to find the smallest of 4 numbers. 
+# a = int(input("Enter Number 1: "))
+# b = int(input("Enter Number 2: "))
+# c = int(input("Enter Number 3: "))
+# d = int(input("Enter Number 4: "))
+
+# if a <= b:
+#     if a <= c:
+#         if a <= d:
+#             print("First number is Smallest")
+#         else:
+#             print("Fourth number is Smallest")
+#     else:
+#         if c <= d:
+#             print("Third number is Smallest")
+#         else:
+#             print("Fourth number is Smallest")
+# else:
+#     if b <= c:
+#         if b <= d:
+#             print("Second number is Smallest")
+#         else:
+#             print("Fourth number is Smallest")
+#     else:
+#         if c <= d:
+#             print("Third number is Smallest")
+#         else:
+#             print("Fourth number is Smallest")
+
+
+
+
+# # 38. Write a program to print middle Character of the given string only if it is upper                
+# #     Case Character.
+
+# a = input("Enter String : ")
+# b = len(a)//2
+# if len(a)%2==1:
+#     if a[b]== a[b].upper():
+#         print(a[b])
+#     else:
+#         print("Not Upper")
+# else:
+#     print("Dont have middle value")
